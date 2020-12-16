6e16c1ff2b3055049d3b00dfa339b09d8b199344
johannabi <jbinnewitt@gmail.com>
2020-12-16 18:20:38 +0100

    implemented entropy

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -1,5 +1,10 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import weka.core.Instance;
 import weka.core.Instances;
 
 public class Classifier {
@@ -11,7 +16,29 @@ public class Classifier {
 
 	public double entropy(Instances instances) {
 		// TODO: implement
-		return 0.0;
+		
+		Enumeration<Instance> instanceEnumeration = instances.enumerateInstances();
+		Map<Double, Integer> classCounter = new HashMap<>();
+		// there must be a weka method that does this...
+		while (instanceEnumeration.hasMoreElements()) {
+			double value = instanceEnumeration.nextElement().classValue();
+			int count = 0;
+			if (classCounter.containsKey(value))
+				count = classCounter.get(value);
+			classCounter.put(value, count+ 1);
+		}
+		
+		int instancesSize = instances.size();	
+		double entropy = 0.0;
+		double relFreq;
+		for(Map.Entry<Double, Integer> e : classCounter.entrySet()) {
+			relFreq = e.getValue() / (double) instancesSize;
+			double current =  relFreq * Math.log(relFreq);
+			entropy += current;
+			
+		}
+		entropy = - entropy;
+		return entropy;
 	}
 
 	/**
@@ -19,6 +46,13 @@ public class Classifier {
 	 */
 	public double informationGain(Instances instances, int attributeIndex) {
 		// TODO: implement
+		double entropyBefore = entropy(instances);
+		// TODO split instances according to attributeValue
+		
+		
+		
+		
+		// TODO compute entropy for each part
 		return 0.0;
 	}
 }
