c02de974a3124025c2e64329edab5694934805fa
krause-s <skrause.koeln@gmail.com>
2020-12-16 19:42:09 +0100

    finished step 4

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -1,6 +1,12 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.UnassignedClassException;
 
 public class Classifier {
 
@@ -10,8 +16,34 @@ public class Classifier {
 	};
 
 	public double entropy(Instances instances) {
-		// TODO: implement
-		return 0.0;
+		try {
+			Map<Double, Double> countMap = new HashMap<Double, Double>();
+			Enumeration<Instance> e = instances.enumerateInstances();
+			
+			while(e.hasMoreElements()) {
+				Instance instance = e.nextElement();
+				double value = instance.classValue();
+
+				if (!countMap.containsKey(value))
+					countMap.put(value, 1d);
+				else
+					countMap.put(value, countMap.get(value)+1);
+			}
+			
+			double entropy = 0.0;
+			double numInstances = instances.numInstances();
+			
+			for (double value : countMap.values()) {
+				double freq = value / numInstances;
+				entropy += (freq * Math.log(freq));
+			}
+			
+			entropy = -entropy;
+			return entropy;
+		} catch (UnassignedClassException e) {
+			instances.setClassIndex(instances.numAttributes() - 1);
+			return entropy(instances);
+		}
 	}
 
 	/**
