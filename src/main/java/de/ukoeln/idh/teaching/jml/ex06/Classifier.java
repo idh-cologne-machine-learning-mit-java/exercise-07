7e7eb26432bafb6d83f32d44955291db9248ba35
MoritzMeid <ameid@smail.uni-koeln.de>
2020-12-17 18:05:07 +0100

    added entropy() and subfunction

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -1,5 +1,7 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import java.util.HashMap;
+
 import weka.core.Instances;
 
 public class Classifier {
@@ -9,11 +11,54 @@ public class Classifier {
 		return null;
 	};
 
-	public double entropy(Instances instances) {
-		// TODO: implement
-		return 0.0;
+	public HashMap<Double, Integer> buildFrequencyTable(Instances instances) {
+		
+		HashMap<Double, Integer> frequencyTable = new HashMap<Double, Integer>();		
+		
+		for(int index = 0; index < instances.numInstances(); index++) {
+			
+			double classValue = instances.get(index).classValue();
+			
+			if(frequencyTable.get(classValue) == null) {
+				
+				frequencyTable.put(classValue, 1);
+				
+			}else {
+				
+				frequencyTable.put(classValue, frequencyTable.get(classValue) + 1);
+				
+			}
+			
+		}
+		
+		return frequencyTable;
+		
 	}
 
+	public double entropy(Instances instances) {
+		
+			if (instances.classIndex() == -1)
+				
+				 instances.setClassIndex(instances.numAttributes() - 1);
+			
+			//build Frequency Table
+			
+			HashMap<Double, Integer> frequencyTable = buildFrequencyTable(instances);
+			
+			// Calculate Entropy
+			
+			double entropy = 0.0;
+			
+			for(int count : frequencyTable.values()) {
+				
+				entropy -= (((double) count / instances.numInstances()) * Math.log((double) count / instances.numInstances()));
+			}
+			
+			System.out.println(entropy);
+		
+			return (double)Math.round(entropy * 1000d) / 1000d;
+		
+		}
 	/**
 	 * calculates information gain for a individual feature
 	 */
