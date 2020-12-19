3b11d358fc9451ce8a91dd3aefb2ff5a71166af0
krause-s <skrause.koeln@gmail.com>
2020-12-19 18:17:33 +0100

    cleaned imports

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -4,7 +4,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.UnassignedClassException;
