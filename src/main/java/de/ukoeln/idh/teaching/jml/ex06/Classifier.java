d6b29a02729d7b2b5869c5a25acaf31d2ac3656a
kbertho1 <k.bertholdt@web.de>
2020-12-21 22:12:02 +0100

    implemented entropy

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -1,5 +1,8 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import java.util.HashMap;
+
+import weka.core.Attribute;
 import weka.core.Instances;
 
 public class Classifier {
@@ -10,9 +13,40 @@ public class Classifier {
 	};
 
 	public double entropy(Instances instances) {
-		// TODO: implement
-		return 0.0;
+		
+		HashMap<Double, Integer> classCounter = new HashMap<Double, Integer>();
+		Attribute classAttribute; 
+		double ent = 0.0;
+		
+		try {
+			classAttribute = instances.classAttribute();
+		} catch (Exception e) {
+			//take last attribute as class attribute
+			classAttribute = instances.attribute(instances.numAttributes() - 1);
+		}
+		
+		//count instances per class
+		for(int i = 0; i < instances.numInstances(); i++) {
+			
+			double classOfInstance = instances.get(i).value(classAttribute);
+			//if no entry, set to 1
+			if(classCounter.get(classOfInstance) == null) {
+				classCounter.put(classOfInstance, 1);
+			}else{
+				classCounter.put(classOfInstance, classCounter.get(classOfInstance) + 1);
+			}
+			
+		}
+		
+		//calculate entropy
+		int numInstances = instances.numInstances();
+		for(int count : classCounter.values()) {
+				double relFreq = count / (double)numInstances;
+				ent += relFreq * Math.log(relFreq);
+		}
+		return ent * -1;
 	}
+		
 
 	/**
 	 * calculates information gain for a individual feature
