1d813069fe38fb0c6fd78ddd6f368a14917f0b6b
MiaDiaDev <mi.dienhart@gmail.com>
2020-12-21 14:02:43 +0100

    implemented entropy

--- src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex06/Classifier.java
@@ -1,5 +1,7 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import java.util.HashMap;
+
 import weka.core.Instances;
 
 public class Classifier {
@@ -9,13 +11,27 @@ public class Classifier {
 		return null;
 	};
 
+	// calculate entropy with set class attribute
 	public double entropy(Instances instances) {
-		// TODO: implement
-		return 0.0;
+		double entropy = 0.0;
+		int countInstances = instances.numInstances();
+		HashMap<Double, Integer> dtMap = new HashMap<Double, Integer>();
+
+		for(int i=0; i < countInstances; i++) {
+			double classAttribute = instances.get(i).classValue();
+			dtMap.put(classAttribute, dtMap.get(classAttribute) + 1);
+		}
+
+		for(double value : dtMap.values()) {
+			double freq = value / countInstances;
+			entropy += (freq * Math.log(freq));
+		}
+
+		return entropy * -1;
 	}
 
 	/**
-	 * calculates information gain for a individual feature
+	 * calculates information gain for an individual feature
 	 */
 	public double informationGain(Instances instances, int attributeIndex) {
 		// TODO: implement
